99934849e17f7a0613a7374f05c53e0463b1b538
Joao Ferreira <jooaooferreira@gmail.com>
2016-11-09 23:09:07 +0100

    Add filtering by search to the post listing view

--- feed/views.py
+++ feed/views.py
@@ -9,12 +9,20 @@ from .serializers import PostSerializer
 class PostViewSet(APICommonMixin, ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_fields = ('search',)
 
     def filter_queryset(self, queryset):
+        # this allows the user to only change his own posts
         if self.request.method == 'GET':
-            return queryset.feed_for_user(self.request.user)
-        if self.request.method in ['DELETE', 'PATCH', 'PUT']:
-            return queryset.from_user(self.request.user)
+            queryset = queryset.feed_for_user(self.request.user)
+        elif self.request.method in ['DELETE', 'PATCH', 'PUT']:
+            queryset = queryset.from_user(self.request.user)
+
+        # search parameters
+        search_param = self.request.query_params.get('search')
+        if search_param:
+            queryset = queryset.filter(partials__text__icontains=search_param)
+
         return queryset
 
     def get_permissions(self):
